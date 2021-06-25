29cb5ad20ef8c64f03cabefcbede9bf9c99d7255
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-06-25 18:01:20 +0600

    Add arguments to command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .version('1.0.0', '-v, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);
